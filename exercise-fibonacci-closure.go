2521d70436840b0ef88a1fd9e99d9bb858d0e9b1
retnuhking <44440921+retnuhking@users.noreply.github.com>
2019-02-27 10:59:05 -0800

    Update exercise-fibonacci-closure.go

--- exercise-fibonacci-closure.go
+++ exercise-fibonacci-closure.go
@@ -6,9 +6,12 @@ import "fmt"
 // a function that returns an int.
 func fibonacci() func() int {
 	s := make([]int, 2)
+	
+	//sets initial two values, required for first addition
 	s[0] = 0
 	s[1] = 1
 	
+	//takes the last two numbers and adds them
 	return func() int {
 		fibReturn := s[len(s)-1]
 		s = append(s, s[len(s)-1] + s[len(s)-2])
